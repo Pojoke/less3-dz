7250c6cc9b19f164fa85856ee27c368677ff2157
Pojoke <pavlykdmutro@gmail.com>
2024-09-14 14:34:48 +0300

    pogoda

--- index.php
+++ index.php
@@ -1,11 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Прогноз погоди </h1>
+   
 <?php
 $x = rand(min: -2, max: 10);
 if ($x < 0) {
-    echo "Мороз";
+    echo "<button><p>Мороз</p></button>";
 } elseif ($x == 0) {
-    echo "Не мороз";
-} else {echo "Тепло";}
+    echo "<button><p>Не мороз</p></button>";
+} else {echo "<button><p>Тепло</p></button>";}
 ?>
+
+
+</body>
+</html>
+
+
+
 
 
 
