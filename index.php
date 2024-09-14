611ce12d931004d6bbd135341d72cd9d7dd0c352
Pojoke <pavlykdmutro@gmail.com>
2024-09-14 14:05:17 +0300

    php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$x = rand(min: -2, max: 10);
+if ($x < 0) {
+    echo "Мороз";
+} elseif ($x == 0) {
+    echo "Не мороз";
+} else {echo "Тепло";}
+?>
+
+
+
+<!-- // echo __FILE__;
+// echo PHP_VERSION; -->
