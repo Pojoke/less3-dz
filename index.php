6126dcb755694af86aae07eed64484c33039df4c
Pojoke <pavlykdmutro@gmail.com>
2024-09-14 20:11:17 +0300

    yes

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
 </head>
 <body>
     <h1>Прогноз погоди </h1>
-   
+
 <?php
 $x = rand(min: -2, max: 10);
+$moroz = rand(min: '-20  ', max: '-1 ');
+$ne_moroz = rand(min: 0, max: '+10 ');
+$teplo = rand(min: '+16  ', max: '+40  ');
 if ($x < 0) {
-    echo "<button><p>Мороз</p></button>";
+    echo "<button><p>Мороз  $moroz </p> </button>";
 } elseif ($x == 0) {
-    echo "<button><p>Не мороз</p></button>";
-} else {echo "<button><p>Тепло</p></button>";}
+    echo "<button><p>Не мороз +$ne_moroz</p></button>";
+} else {echo "<button><p>Тепло +$teplo</p></button>";}
 ?>
 
 
